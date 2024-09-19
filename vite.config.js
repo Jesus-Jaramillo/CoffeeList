da94c1fb5ba72b46025d207ade5dadc2a8c35db4
Jesus Jaramillo <jesusjaramillo2002@gmail.com>
2024-09-19 16:25:30 -0500

    archive vite uptade

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,4 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  /* base: "https://Jesus-Jaramillo.github.io/Simple-Coffee-Listing" */
 })
